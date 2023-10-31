ed81792a50c582ffc22d8c6fac74d4fc0af04640
moinulpalmal <moinul.it@palmalgarments.com>
2023-10-31 18:24:48 +0600

    Employee List Done #1

--- app/Model/issueType.php
+++ app/Model/issueType.php
@@ -3,6 +3,7 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class issueType extends Model
@@ -22,4 +23,109 @@ class issueType extends Model
             ->orderBy('name', 'ASC')
             ->pluck("id","name");
     }
+
+    public static function allActiveIssueTypes(){
+        return DB::table('issue_types')
+            ->select('id', 'name', 'status')
+            ->where('status', '=', 'A')
+            ->orderBy('name', 'ASC')
+            ->get();
+    }
+
+    public static function allDeletedIssueTypes(){
+        return DB::table('issue_types')
+            ->select('id', 'name', 'status')
+            ->where('status', '=', 'D')
+            ->orderBy('name', 'ASC')
+            ->get();
+    }
+
+    public static function allNotDeletedIssueTypes(){
+        return DB::table('issue_types')
+            ->select('id', 'name', 'status')
+            ->where('status', '!=', 'D')
+            ->orderBy('name', 'ASC')
+            ->get();
+    }
+
+    public static function insertIssueTypes($request){
+        $model = new IssueType();
+        $model->name = $request->name;
+        $model->inserted_by = Auth::id();
+        $model->status = 'A';
+        if($model->save()){
+
+            return '1';
+        }
+        return '0';
+    }
+
+    public static function updateIssueTypes($request){
+        $model = IssueType::find($request->id);
+        if($model != null){
+            $model->name = $request->name;
+            $model->last_updated_by = Auth::id();
+
+            if($model->save()){
+                return '2';
+            }
+            return '0';
+        }
+
+        return '0';
+    }
+
+    public static function returnForEdit($id){
+        $model = IssueType::find($id);
+        if($model != null){
+            $data = array(
+                'name' => $model->name,
+                'id' => $model->id
+            );
+            return $data;
+        }
+        return '0';
+
+    }
+
+    public static function returnDelete($id){
+        $model = IssueType::find($id);
+
+        if($model != null){
+            $model->status = 'D';
+            if($model->save()){
+                return '1';
+            }
+
+            return '0';
+        }
+        return '0';
+    }
+
+    public static function returnActivate($id){
+        $model = IssueType::find($id);
+
+        if($model != null){
+            $model->status = 'A';
+            if($model->save()){
+                return '1';
+            }
+            return '0';
+        }
+        return '0';
+    }
+
+    public static function returnDeActivate($id){
+        $model = IssueType::find($id);
+        if($model != null){
+            $model->status = 'I';
+            if($model->save()){
+                return '1';
+            }
+            return '0';
+        }
+        return '0';
+    }
+
+
 }
