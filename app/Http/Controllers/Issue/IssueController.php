e4044186bec8f28c5e125a4444634236f440fa2b
pitashpalmal <pitash.it@palmalgarments.com>
2023-11-06 17:44:30 +0600

    Issue old individual issue form edit #1

--- app/Http/Controllers/Issue/IssueController.php
+++ app/Http/Controllers/Issue/IssueController.php
@@ -8,8 +8,13 @@ use App\Model\ProductCategory;
 use App\Model\ProductDetail;
 use App\Model\ProductIssue;
 use App\Model\ProductMaster;
+use App\Model\Customer;
+use App\Model\Factory;
+use App\Model\Department;
+use App\Model\Designation;
 use App\Model\ProductSubCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IssueController extends Controller
 {
@@ -25,10 +30,13 @@ class IssueController extends Controller
 
     public function editOldEntry($id){
         $product_issue = ProductIssue::find($id);
-      //  return $product_issue;
 
         if($product_issue){
             if($product_issue->status != 'D'){
+
+                $factories = Factory::allFactoriesForSelectField();
+                $departments = Department::allDepartmentsForSelectField();
+                $designations = Designation::allDesignationsForSelectField();
                 $product_category_id = ProductMaster::getCategoryId($product_issue->product_master_id);
                 $product_sub_category_id = ProductMaster::getSubCategoryId($product_issue->product_master_id);
                 $product_categories = ProductCategory::allProductCategoriesForSelectField();
@@ -37,9 +45,29 @@ class IssueController extends Controller
                 $product_details = ProductDetail::allProductDetailForSelectField();
                 $issue_types = issueType::getListForSlect();
 
-                return view('issue.old-entry.edit', compact('id','issue_types',
-                'product_category_id', 'product_sub_category_id', 'product_categories', 'product_sub_categories',
-                'product_masters', 'product_details', ));
+
+                $customer = DB::table('product_issues')
+                        ->join('customers','customers.id', '=', 'product_issues.customer_id')
+                        ->select('customers.id', 'customers.employee_id','customers.factory','customers.designation','customers.department','customers.name','customers.email',
+                            'customers.mobile_no','customers.ext_no','customers.mobile_no','customers.job_location','customers.status')
+                        ->where('customers.id',$product_issue->customer_id)
+                        ->get();
+
+
+                // if($product_issue->product_detail_id !== 0)
+                //     {
+                //         $product_info = DB::table('product_issues')
+                //                 ->join('product_details','product_details.product_master', '=', 'product_issues.id')
+                //                 ->join('product_masters','product_masters.id', '=', 'product_issues.product_master_id')
+                //                 ->select('product_masters.id','product_masters.name','product_details.sl_no','product_details.purchase_date','product_details.warranty_in_months')
+                //                 ->where('product_masters.id',$product_issue->product_detail_id)
+                //                 ->get();
+                //     }
+
+                // else $product_info = 0;
+
+                return view('issue.old-entry.edit', compact('id','issue_types','product_issue','product_category_id', 'product_sub_category_id',
+                 'product_categories', 'product_sub_categories','product_masters', 'product_details','customer','factories','departments','designations'));
             }
         }
 
